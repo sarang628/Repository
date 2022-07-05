ddc87e089390df0f6af1b5e57e9f0ae594667962
sryang <sarang628@naver.com>
2022-07-05 13:53:29 +0900

    테스트저장소 DB의존성 제공 제거 Remove test repository DB dependency provision

--- TorangRepository/src/main/java/com/example/torangrepository/di/DatabaseModule.kt
+++ TorangRepository/src/main/java/com/example/torangrepository/di/DatabaseModule.kt
@@ -1,25 +1,22 @@
 package com.example.torangrepository.di
 
-import android.content.Context
 import com.example.torang_core.data.AppDatabase
 import com.example.torang_core.data.dao.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class DatabaseModule {
 
     /** 로컬 데이터베이스 제공 */
-    @Singleton
+    /*@Singleton
     @Provides
     fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
         return AppDatabase.getInstance(context)
-    }
+    }*/
 
     /** 로컬 데이터베이스의 사용자 관리 DAO 제공 */
     @Provides
