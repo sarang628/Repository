e338c47f028a84d956fa5bffc7a3d96016d8d775
sryang <sarang628@naver.com>
2022-07-05 13:55:54 +0900

    내 리뷰를 가져오는 기능 테스트 Functional tests that get my reviews

--- TorangRepositoryTestApp/app/src/main/java/com/example/torangrepositorytestapp/MainActivity.kt
+++ TorangRepositoryTestApp/app/src/main/java/com/example/torangrepositorytestapp/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.torangrepositorytestapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.torang_core.repository.FeedRepository
+import com.example.torang_core.repository.LoginRepository
+import com.example.torang_core.repository.MyReviewsRepository
+import com.example.torang_core.util.Logger
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
@@ -13,12 +22,30 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var feedRepository: FeedRepository
 
+    @Inject
+    lateinit var userRepository: LoginRepository
+
+    @Inject
+    lateinit var myReviewsRepository: MyReviewsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycleScope.launch {
+            /*repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                userRepository.isLoginFlow().collect {
+                    Logger.d("" + it)
+                }
+            }*/
+        }
 
         runBlocking {
-            feedRepository.loadFeed()
+            findViewById<TextView>(R.id.tv).text =
+                myReviewsRepository.getMyReviews(10).size.toString()
+
+            myReviewsRepository.getMyReviews1(0).observe(this@MainActivity) {
+                Logger.d(it.toString())
+            }
         }
     }
 }
