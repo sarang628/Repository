580de775a041f0f1733320b21da728319aa98381
Sarang Yang <sarang628@naver.com>
2022-07-09 13:28:06 +0900

    Find 화면 기능 최적화
    Optimize the Find screen function

--- TorangRepository/src/main/java/com/example/torangrepository/FindRepositoryImpl.kt
+++ TorangRepository/src/main/java/com/example/torangrepository/FindRepositoryImpl.kt
@@ -1,8 +1,16 @@
 package com.example.torangrepository
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import com.example.torang_core.data.LocationPreferences
 import com.example.torang_core.data.dao.RestaurantDao
 import com.example.torang_core.data.model.*
 import com.example.torang_core.repository.FindRepository
+import com.example.torang_core.repository.RequestLocationResult
+import com.example.torang_core.util.Logger
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,6 +28,37 @@ class FindRepositoryImpl @Inject constructor(
 
     val restaurants = MutableStateFlow<List<Restaurant>>(ArrayList())
 
+@Singleton
+class FindRepositoryImpl @Inject constructor(
+    private val restaurantDao: RestaurantDao,
+    private val restaurantService: RestaurantService,
+    private val locationPreferences: LocationPreferences,
+    @ApplicationContext private val context: Context
+) : FindRepository {
+    // 최초 위치요쳥을 false로 설정 시 화면단에서 요청해야함
+    private val isFirstRequestLocation = MutableStateFlow(false)
+    private val isRequestingLocation = MutableStateFlow(false)
+    private val currentPosition = MutableStateFlow(0)
+    private val showRestaurantCardAndFilter = MutableStateFlow(true)
+
+    // 위치요청을 처음 했는지 여부
+    private val isFirstRequestLocationPermission = MutableStateFlow(false)
+
+    // 권한이 있는지 여부
+    private val hasGrantPermission: MutableStateFlow<Int> =
+        MutableStateFlow<Int>(context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION))
+
+    private val restaurants = MutableStateFlow<List<Restaurant>>(ArrayList())
+
+    private val manager: LocationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    override suspend fun searchIfRestaurantEmpty(){
+        if(restaurants.value.isEmpty()){
+            restaurants.emit(restaurantService.getAllRestaurant(HashMap()))
+        }
+    }
+
     /**
      * 화면 첫 진입 시 위치를 요청해야하는지에 대한 상태
      */
@@ -28,11 +67,32 @@ class FindRepositoryImpl @Inject constructor(
     }
 
     /**
-     * 위치를 요청했다고 알려줌
+     * 다른화면과의 상태 공유를 위해 저장소에서 위치요청을 했다고 알려줘야합니다.
      */
-    override suspend fun notifyRequestLocation() {
+    override suspend fun notifyRequestLocation(): RequestLocationResult {
+        // 위치 요청이 처음이라면 권한 필요 팝업 요청
+        if (!locationPreferences.isFirstRequestLocationPermission()) {
+            locationPreferences.requestLocationPermission()
+            return RequestLocationResult.NEED_LOCATION_PERMISSION
+        }
+
+        // 첫 권한 필요 팝업을 거부했거나 권한이 없을경우 다른 권한 필요 팝업 요청
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            return RequestLocationResult.PERMISSION_DENIED
+        }
+
+        val statusOfGPS: Boolean = manager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (!statusOfGPS) {
+            return RequestLocationResult.GPS_OFF
+        }
+
+        // Map화면과 공유하기위한 상태 변경
         isFirstRequestLocation.emit(true)
         isRequestingLocation.emit(true)
+
+        //성공 리턴
+        return RequestLocationResult.SUCCESS
     }
 
     /**
@@ -47,6 +107,39 @@ class FindRepositoryImpl @Inject constructor(
      */
     override suspend fun notifyReceiveLocation() {
         isRequestingLocation.emit(false)
+    }
+
+    override suspend fun setCurrentPosition(position: Int) {
+        currentPosition.emit(position)
+        //추가 카드 보여지게 변경
+        if (!showRestaurantCardAndFilter.value)
+            showRestaurantCardAndFilter.emit(true)
+    }
+
+    override fun getCurrentPosition(): StateFlow<Int> {
+        return currentPosition
+    }
+
+    override suspend fun requestLocationPermission(b: Boolean) {
+        locationPreferences.requestLocationPermission()
+        isFirstRequestLocationPermission.emit(locationPreferences.isFirstRequestLocationPermission())
+    }
+
+    override fun hasGrantPermission(): MutableStateFlow<Int> {
+        return hasGrantPermission
+    }
+
+    override suspend fun permissionGranated() {
+        Logger.d("check emit hasGrantPermission = ${hasGrantPermission.value}, PackageManager.PERMISSION_GRANTED = ${PackageManager.PERMISSION_GRANTED}")
+        hasGrantPermission.emit(PackageManager.PERMISSION_GRANTED)
+    }
+
+    override fun showRestaurantCardAndFilter(): StateFlow<Boolean> {
+        return showRestaurantCardAndFilter
+    }
+
+    override suspend fun clickMap() {
+        showRestaurantCardAndFilter.emit(!showRestaurantCardAndFilter.value)
     }
 
     override fun getSearchedRestaurant(): Flow<List<Restaurant>> {
@@ -64,7 +157,7 @@ class FindRepositoryImpl @Inject constructor(
         northEastLongitude: Double,
         southWestLatitude: Double,
         southWestLongitude: Double,
-        searchType: Filter.SearchType
+        searchType: SearchType
     ) {
         val filter = Filter().apply {
             distances?.let { this.distances = it }
--- TorangRepository/src/main/java/com/example/torangrepository/TorangRepository.kt
+++ TorangRepository/src/main/java/com/example/torangrepository/TorangRepository.kt
@@ -3,16 +3,21 @@ package com.example.torangrepository
 import android.content.Context
 import androidx.lifecycle.LiveData
 import com.example.torang_core.data.dao.RestaurantDao
+import com.example.torang_core.data.data.Location
 import com.example.torang_core.data.model.*
 import com.example.torang_core.repository.InfoRepository
 import com.example.torang_core.repository.MapRepository
 import com.example.torang_core.repository.MenuRepository
 import com.example.torang_core.repository.ReviewRepository
+import com.example.torang_core.util.Logger
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -25,6 +30,8 @@ class TorangRepository @Inject constructor(
     InfoRepository, ReviewRepository, MenuRepository,
     MapRepository {
 
+    private val mapClick = MutableStateFlow<Boolean>(false)
+    private val location = MutableStateFlow(Location(0.0, 0.0))
 
     override suspend fun loadRestaurant(restaurantId: Int): Restaurant {
         return restaurantService.getRestaurant(HashMap<String, String>().apply {
@@ -67,6 +74,64 @@ class TorangRepository @Inject constructor(
             data.add(RestaurantData.parse(restaurant))
         }
         restaurantDao.insertAllRestaurant(data)
+    }
+
+    override fun getClickMap(): Flow<Boolean> {
+        return mapClick
+    }
+
+    override suspend fun clickMap() {
+        mapClick.emit(!mapClick.value)
+    }
+
+    var _northEastLatitude = 0.0
+    override fun setNorthEastLatitude(latitude: Double) {
+        _northEastLatitude = latitude
+    }
+
+    var _notthEastLongitude = 0.0
+    override fun setNorthEastLongitude(longitude: Double) {
+        _notthEastLongitude = longitude
+    }
+
+    var _southWestLatitude = 0.0
+    override fun setSouthWestLatitude(latitude: Double) {
+        _southWestLatitude = latitude
+    }
+
+    var _southWestLongitude = 0.0
+    override fun setSouthWestLongitude(longitude: Double) {
+        _southWestLongitude = longitude
+    }
+
+    override fun getNorthEastLatitude(): Double {
+        return _northEastLatitude
+    }
+
+    override fun getNorthEastLongitude(): Double {
+        return _notthEastLongitude
+    }
+
+    override fun getSouthWestLatitude(): Double {
+        return _southWestLatitude
+    }
+
+    override fun getSouthWestLongitude(): Double {
+        return _southWestLongitude
+    }
+
+    override suspend fun showCard() {
+        if (mapClick.value)
+            mapClick.emit(false)
+    }
+
+    override suspend fun setCurrentLocation(location: Location) {
+        Logger.d("lat : ${location.latitude} , lon : ${location.longitude}")
+        this.location.emit(location)
+    }
+
+    override fun getCurrentLocationFlow(): StateFlow<Location> {
+        return location
     }
 }
 
